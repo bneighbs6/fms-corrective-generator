df848ead869bd53eb73a19a5ee80d6835ac07ead
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-10-09 15:45:52 -0600

    Add useNavigate to travel to /new-fms webpage

--- src/components/FMSCard/NewFMSCard.jsx
+++ src/components/FMSCard/NewFMSCard.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import "./newFMSCard.css"; // All extra styling not from Bootstrap is here
 
 function NewFMSCard() {
+  const navigate = useNavigate(); 
     // This will be placed on the home page as an option for the user -> it will bring the user to a new page with an FMS Score Sheet
     return (
       <div className="container">
@@ -10,7 +12,7 @@ function NewFMSCard() {
           <Card.Body>
             <Card.Title>New FMS Score Sheet</Card.Title>
             <Card.Text>This is for a new FMS with a client you haven't seen before</Card.Text>
-            <Button>Go to New Web Page with FMS Scoresheet</Button>
+            <Button onClick={() => navigate("/new-fms")}>Go to New Web Page with FMS Scoresheet</Button>
           </Card.Body>
         </Card>
       </div>
