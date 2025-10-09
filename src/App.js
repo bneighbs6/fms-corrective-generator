09f158600683887232bace214eb3f9c03b892596
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-10-09 14:49:22 -0600

    Install react-router-dom & create routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import HomePage from './components/HomePage';
+import FMSScoresheet from './components/FMSScoreSheet/FMSScoresheet';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const header = <h1>This is my Functional Movement Screen Corrective Generator</h1>
   return (
-    <div className="App">
-        <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
