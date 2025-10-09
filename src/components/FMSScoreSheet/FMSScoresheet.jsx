9c9683da90c002978dd373b4f154165633e6751e
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-10-09 13:36:46 -0600

    Create initalFormState and handleChange function

--- src/components/FMSScoreSheet/FMSScoresheet.jsx
+++ src/components/FMSScoreSheet/FMSScoresheet.jsx
@@ -1,20 +1,40 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Card, Table, Form, Container } from "react-bootstrap";
-import "./FMSScoresheet.css"
+import "./FMSScoresheet.css";
 
 function FMSScoresheet() {
-  const [deepSquat, setDeepSquat] = useState("");
-  const [leftHurdleStep, setLeftHurdleStep] = useState("");
-  const [rightHurdleStep, setRightHurdleStep] = useState("");
+  const initialFMSScoresheetState = {
+    deepSquat: "",
+    leftHurdleStep: "",
+    rightHurdleStep: "",
+    leftInlineLunge: "",
+    rightInlineLunge: "",
+    leftShoulderMobility: "",
+    rightShoulderMobility: "",
+    leftShoulderClearing: "",
+    rightShoulderClearing: "",
+    leftActiveStraightLegRaise: "",
+    rightActiveStraightLegRaise: "",
+    trunkStabilityPushUp: "",
+    extensionClearing: "",
+    leftRotaryStability: "",
+    rightRotaryStability: "",
+    flexionClearing: "",
+    totalScore: "",
+  };
 
-const handleDeepSquatChange = (e) => setDeepSquat(e.target.value);
-const handleLeftHurdleStepChange = (e) => setLeftHurdleStep(e.target.value);
-const handleRightHurdleStepChange = (e) => setRightHurdleStep(e.target.value);
+  const [fmsScoresheetData, setFMSScoresheetData] = useState({
+    ...initialFMSScoresheetState,
+  });
 
-console.log("the current value of deep squat is " + deepSquat);
-console.log("The current value of Left hurdle step is " + leftHurdleStep);
-console.log("the current value of Right Hrudle Step is " + rightHurdleStep)
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFMSScoresheetData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="fms-scoresheet">
       <Container>
@@ -34,14 +54,16 @@ console.log("the current value of Right Hrudle Step is " + rightHurdleStep)
                 </tr>
               </thead>
               <tbody>
-                {/* Deep Squat Section */}
+                {/* Deep Squat */}
                 <tr>
                   <td>Deep Squat</td>
                   <td>
                     <Form.Control
                       type="number"
                       placeholder="Raw"
-                      onChange={handleDeepSquatChange}
+                      name="deepSquat"
+                      value={fmsScoresheetData.deepSquat}
+                      onChange={handleFormChange}
                     />
                   </td>
                   <td>
@@ -51,19 +73,24 @@ console.log("the current value of Right Hrudle Step is " + rightHurdleStep)
                     <Form.Control type="text" placeholder="Comments" />
                   </td>
                 </tr>
-                {/* Hurdle Step Section */}
+
+                {/* Hurdle Step */}
                 <tr>
                   <td>Hurdle Step</td>
                   <td>
                     <Form.Control
                       type="number"
                       placeholder="Left"
-                      onChange={handleLeftHurdleStepChange}
+                      name="leftHurdleStep"
+                      value={fmsScoresheetData.leftHurdleStep}
+                      onChange={handleFormChange}
                     />
                     <Form.Control
                       type="number"
                       placeholder="Right"
-                      onChange={handleRightHurdleStepChange}
+                      name="rightHurdleStep"
+                      value={fmsScoresheetData.rightHurdleStep}
+                      onChange={handleFormChange}
                     />
                   </td>
                   <td>
@@ -73,12 +100,25 @@ console.log("the current value of Right Hrudle Step is " + rightHurdleStep)
                     <Form.Control type="text" placeholder="Comments" />
                   </td>
                 </tr>
-                {/* Inline Lunge Section */}
+
+                {/* Inline Lunge */}
                 <tr>
                   <td>Inline Lunge</td>
                   <td>
-                    <Form.Control type="number" placeholder="Left" />
-                    <Form.Control type="number" placeholder="Right" />
+                    <Form.Control
+                      type="number"
+                      placeholder="Left"
+                      name="leftInlineLunge"
+                      value={fmsScoresheetData.leftInlineLunge}
+                      onChange={handleFormChange}
+                    />
+                    <Form.Control
+                      type="number"
+                      placeholder="Right"
+                      name="rightInlineLunge"
+                      value={fmsScoresheetData.rightInlineLunge}
+                      onChange={handleFormChange}
+                    />
                   </td>
                   <td>
                     <Form.Control type="number" placeholder="Final" />
@@ -87,12 +127,25 @@ console.log("the current value of Right Hrudle Step is " + rightHurdleStep)
                     <Form.Control type="text" placeholder="Comments" />
                   </td>
                 </tr>
-                {/* Shoulder Mobility Section */}
+
+                {/* Shoulder Mobility */}
                 <tr>
                   <td>Shoulder Mobility</td>
                   <td>
-                    <Form.Control type="number" placeholder="Left" />
-                    <Form.Control type="number" placeholder="Right" />
+                    <Form.Control
+                      type="number"
+                      placeholder="Left"
+                      name="leftShoulderMobility"
+                      value={fmsScoresheetData.leftShoulderMobility}
+                      onChange={handleFormChange}
+                    />
+                    <Form.Control
+                      type="number"
+                      placeholder="Right"
+                      name="rightShoulderMobility"
+                      value={fmsScoresheetData.rightShoulderMobility}
+                      onChange={handleFormChange}
+                    />
                   </td>
                   <td>
                     <Form.Control type="number" placeholder="Final" />
@@ -101,20 +154,50 @@ console.log("the current value of Right Hrudle Step is " + rightHurdleStep)
                     <Form.Control type="text" placeholder="Comments" />
                   </td>
                 </tr>
-                {/* Shoulder Clearing Section */}
+
+                {/* Shoulder Clearing */}
                 <tr>
                   <td>Shoulder Clearing</td>
                   <td>
-                    <Form.Control type="text" placeholder="Left +/-" />
-                    <Form.Control type="text" placeholder="Right +/-" />
+                    <Form.Control
+                      type="text"
+                      placeholder="Left +/-"
+                      name="leftShoulderClearing"
+                      value={fmsScoresheetData.leftShoulderClearing}
+                      onChange={handleFormChange}
+                    />
+                    <Form.Control
+                      type="text"
+                      placeholder="Right +/-"
+                      name="rightShoulderClearing"
+                      value={fmsScoresheetData.rightShoulderClearing}
+                      onChange={handleFormChange}
+                    />
+                  </td>
+                  <td></td>
+                  <td>
+                    <Form.Control type="text" placeholder="Comments" />
                   </td>
                 </tr>
+
                 {/* Active Straight-Leg Raise */}
                 <tr>
                   <td>Active Straight-Leg Raise</td>
                   <td>
-                    <Form.Control type="number" placeholder="Left" />
-                    <Form.Control type="number" placeholder="Right" />
+                    <Form.Control
+                      type="number"
+                      placeholder="Left"
+                      name="leftActiveStraightLegRaise"
+                      value={fmsScoresheetData.leftActiveStraightLegRaise}
+                      onChange={handleFormChange}
+                    />
+                    <Form.Control
+                      type="number"
+                      placeholder="Right"
+                      name="rightActiveStraightLegRaise"
+                      value={fmsScoresheetData.rightActiveStraightLegRaise}
+                      onChange={handleFormChange}
+                    />
                   </td>
                   <td>
                     <Form.Control type="number" placeholder="Final" />
@@ -123,11 +206,18 @@ console.log("the current value of Right Hrudle Step is " + rightHurdleStep)
                     <Form.Control type="text" placeholder="Comments" />
                   </td>
                 </tr>
+
                 {/* Trunk Stability Push Up */}
                 <tr>
                   <td>Trunk Stability Push Up</td>
                   <td>
-                    <Form.Control type="number" placeholder="Raw" />
+                    <Form.Control
+                      type="number"
+                      placeholder="Raw"
+                      name="trunkStabilityPushUp"
+                      value={fmsScoresheetData.trunkStabilityPushUp}
+                      onChange={handleFormChange}
+                    />
                   </td>
                   <td>
                     <Form.Control type="number" placeholder="Final" />
@@ -136,19 +226,43 @@ console.log("the current value of Right Hrudle Step is " + rightHurdleStep)
                     <Form.Control type="text" placeholder="Comments" />
                   </td>
                 </tr>
+
                 {/* Extension Clearing */}
                 <tr>
                   <td>Extension Clearing</td>
                   <td>
-                    <Form.Control type="text" placeholder="+/-" />
+                    <Form.Control
+                      type="text"
+                      placeholder="+/-"
+                      name="extensionClearing"
+                      value={fmsScoresheetData.extensionClearing}
+                      onChange={handleFormChange}
+                    />
+                  </td>
+                  <td></td>
+                  <td>
+                    <Form.Control type="text" placeholder="Comments" />
                   </td>
                 </tr>
+
                 {/* Rotary Stability */}
                 <tr>
                   <td>Rotary Stability</td>
                   <td>
-                    <Form.Control type="number" placeholder="Left" />
-                    <Form.Control type="number" placeholder="Right" />
+                    <Form.Control
+                      type="number"
+                      placeholder="Left"
+                      name="leftRotaryStability"
+                      value={fmsScoresheetData.leftRotaryStability}
+                      onChange={handleFormChange}
+                    />
+                    <Form.Control
+                      type="number"
+                      placeholder="Right"
+                      name="rightRotaryStability"
+                      value={fmsScoresheetData.rightRotaryStability}
+                      onChange={handleFormChange}
+                    />
                   </td>
                   <td>
                     <Form.Control type="number" placeholder="Final" />
@@ -157,17 +271,39 @@ console.log("the current value of Right Hrudle Step is " + rightHurdleStep)
                     <Form.Control type="text" placeholder="Comments" />
                   </td>
                 </tr>
-                {/* Total Testing Score */}
+
+                {/* Flexion Clearing */}
+                <tr>
+                  <td>Flexion Clearing</td>
+                  <td>
+                    <Form.Control
+                      type="text"
+                      placeholder="+/-"
+                      name="flexionClearing"
+                      value={fmsScoresheetData.flexionClearing}
+                      onChange={handleFormChange}
+                    />
+                  </td>
+                  <td></td>
+                  <td>
+                    <Form.Control type="text" placeholder="Comments" />
+                  </td>
+                </tr>
+
+                {/* Total Screen Score */}
                 <tr>
                   <td>Total Screen Score</td>
+                  <td></td>
                   <td>
                     <Form.Control
                       type="number"
                       placeholder="Final Test Score"
+                      name="totalScore"
+                      value={fmsScoresheetData.totalScore}
+                      onChange={handleFormChange}
                     />
                   </td>
                 </tr>
-                {/* repeat for other tests */}
               </tbody>
             </Table>
           </Card.Body>
