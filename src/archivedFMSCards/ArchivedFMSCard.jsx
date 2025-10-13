61b639e4c4929af1c40a47a47f3ea555abc7aed9
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-10-13 11:41:15 -0600

    Style ArchivedFMSCard

--- src/archivedFMSCards/ArchivedFMSCard.jsx
+++ src/archivedFMSCards/ArchivedFMSCard.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import "./ArchivedFMSCard.css";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Container } from "react-bootstrap";
 
 function ArchivedFMSCard() {
     return (
-      <Card>
-        <Card.Body>
-          <Card.Title>Archived FMS Scoresheets</Card.Title>
-          <Card.Text>Click below to view archived FMS Scoresheets</Card.Text>
-          <Button onClick={() => console.log("Archived FMS Button Clicked")}>Archived FMS Scoresheets</Button>
-        </Card.Body>
-      </Card>
+      <div>
+        <Container>
+          <Card>
+            <Card.Body>
+              <Card.Title>Archived FMS Scoresheets</Card.Title>
+              <Card.Text>
+                Click below to view archived FMS Scoresheets
+              </Card.Text>
+              <Button
+                onClick={() => console.log("Archived FMS Button Clicked")}
+              >
+                Archived FMS Scoresheets
+              </Button>
+            </Card.Body>
+          </Card>
+        </Container>
+      </div>
     );
 }
 
