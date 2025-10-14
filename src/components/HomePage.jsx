01079595eceb5d3cb2ee5a57e52a2a686385ed87
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-10-14 11:06:35 -0600

    Move ArchivedFMSCard into components folder

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NewFMSCard from "./FMSCard/NewFMSCard";
-import ArchivedFMSCard from "../archivedFMSCards/ArchivedFMSCard";
+import ArchivedFMSCard from "./archivedFMSCards/ArchivedFMSCard";
 
 // TODO: NewFMSCard needs to have the final score auto tallied by the test's raw scores
 // Develop backend in order to store data for ArchivedFMSCard
